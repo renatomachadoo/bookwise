b7e1932b569e429aec3c6422cd689238122ea9ae
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2025-01-20 22:08:27 +0000

    chore: replace manual font imports with next/font API

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,18 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nunito.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -5,18 +5,6 @@ export default function Document() {
   return (
     <Html lang="pt">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wdth,wght,YTLC@0,6..12,75..125,200..1000,440..540;1,6..12,75..125,200..1000,440..540&display=swap"
-          rel="stylesheet"
-        />
-
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
