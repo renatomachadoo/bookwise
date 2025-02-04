7d857abd1694bcc83b12ab57966ba2da8620cb28
Renato Machado <renatomachado176@gmail.com>
2025-02-04 19:25:41 +0000

    chore: changed configuration files to deploy

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
-  useFileSystemPublicRoutes: false,
 }
 
 export default nextConfig
